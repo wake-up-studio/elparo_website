6952c021810ac37a93e713c61be39c53c28e4fa1
Wake Up..! <wakeuphuis@gmail.com>
2026-06-23 11:10:40 +0200

    Create elparo_projects_script.js

---
+++ elparo_projects_script.js
@@ -0,0 +1,148 @@
+document.addEventListener("DOMContentLoaded", (event) => {
+
+//**********************************************************************************
+//TRANSITION
+//**********************************************************************************
+  document.querySelectorAll('a').forEach(link => {
+        link.addEventListener('click', (e) => {
+            const href = link.getAttribute('href');
+
+            if(!href || href.startsWith("#") || href === window.location.pathname) {
+                return;
+            }
+
+            e.preventDefault();
+
+            animateTransition().then (() => {
+                window.location.href = href;
+            });
+        });
+    });
+
+    gsap.set(".transition", {
+        visibility: "visible",
+        translateY: 0,
+    })  //set pour anim intro
+
+    revealTransition().then (() => {
+        gsap.set(".transition", { visibility: "hidden" });
+    }) //réalise anim intro
+
+    function revealTransition() {
+        return new Promise(resolve => {
+            const tl = gsap.timeline({
+                onComplete: resolve,
+            });
+            tl.fromTo(
+                ".transition",
+                {translateY: 0},
+                {
+                    translateY: "-100vh",
+                    duration: 1.5,
+                    delay: 0.2,
+                    stagger: {
+                        each: 0.1,
+                        from: "start",
+                        grid: [1,3],
+                    },
+                    ease: "expo.inOut",
+                },
+                0
+            );
+        });
+    }
+
+    function animateTransition() {
+        return new Promise(resolve => {
+            gsap.set(".transition", {
+                visibility: "visible",
+                translateY: "-100vh",
+            });
+
+            const tl = gsap.timeline({
+                onComplete: resolve,
+            });
+
+            tl.fromTo(
+                ".transition",
+                {translateY: "-100vh"},
+                {
+                    translateY: 0,
+                    duration: 1,
+                    stagger: {
+                        each: 0.1,
+                        from: "start",
+                        grid: [1,3],
+                    },
+                    ease: "expo.out",
+                },
+                0
+            );
+        });
+    }
+
+//**********************************************************************************
+//HOVER
+//**********************************************************************************
+   const projets = document.querySelectorAll(".projetFromProjets");
+
+    projets.forEach( projet => {
+        projet.addEventListener('mouseenter', () => {
+            animateTransitionHover().then( () => {});
+        });
+        projet.addEventListener('mouseleave', () => {
+            revealTransitionHover().then( () => {});
+        })
+    })
+
+    function revealTransitionHover() {
+        return new Promise(resolve => {
+            const tl = gsap.timeline({
+                onComplete: resolve,
+            });
+
+            tl.fromTo(
+                ".hover_projets",
+                {translateY: "0"},
+                {
+                    translateY: "100%",
+                    duration: 1,
+                    stagger: {
+                        each: 0.1,
+                        from: "start",
+                        grid: [1, 3],
+                    },
+                    ease: "expo.inOut",
+                },
+                0
+            );
+        })
+    }
+
+    function animateTransitionHover() {
+        return new Promise(resolve => {
+            const tl = gsap.timeline({
+                onComplete: resolve,
+            });
+
+            tl.fromTo(
+                ".hover_projets",
+                {translateY: "-100%"},
+                {
+                    translateY: 0,
+                    duration: 1,
+                    stagger: {
+                        each: 0.1,
+                        from: "start",
+                        grid: [1, 3],
+                    },
+                    ease: "expo.out",
+                },
+                0
+            );
+        })
+    }
+
+  
+});
+  
