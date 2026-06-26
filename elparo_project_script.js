edc78c6d795d8c77087f8fd732e838409a89cbc2
Glitch Blizzard <dylan.cozian@gmail.com>
2026-06-26 09:40:30 +0200

    Update elparo_project_script.js

--- elparo_project_script.js
+++ elparo_project_script.js
@@ -112,6 +112,23 @@ document.addEventListener("DOMContentLoaded", (event) => {
   }
 
 //**********************************************************************************
+//BATCH
+//**********************************************************************************
+    gsap.set(".item_container", {y: 100});
+
+    ScrollTrigger.batch(".item_container", {
+        //interval: 0.1, // time window (in seconds) for batching to occur.
+        onEnter: batch => gsap.to(batch, {opacity: 1, y: 0, stagger: {each: 0.15, grid: [1, 3]}, overwrite: true}),
+        onLeave: batch => gsap.set(batch, {opacity: 0, y: -100, overwrite: true}),
+        onEnterBack: batch => gsap.to(batch, {opacity: 1, y: 0, stagger: 0.15, overwrite: true}),
+        onLeaveBack: batch => gsap.set(batch, {opacity: 0, y: 100, overwrite: true})
+    });
+    
+    ScrollTrigger.addEventListener("refreshInit", () => gsap.set(".image", {y: 0}));
+    }
+});
+
+//**********************************************************************************
 //GENERATION CONTAINER VIDEO
 //**********************************************************************************
   const video_content = document.querySelector(".video_content");
