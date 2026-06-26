fd28f4aa5d80b925a25ca7b6c70ade6a6c26fced
Glitch Blizzard <dylan.cozian@gmail.com>
2026-06-26 09:39:26 +0200

    Update elparo_projects_script.js

--- elparo_projects_script.js
+++ elparo_projects_script.js
@@ -366,6 +366,21 @@ const tags = gsap.utils.toArray(".tag_scrollbar");
             return () => window.removeEventListener("resize", onResize); // cleanup
         });
         return timeline;
+
+//**********************************************************************************
+//BATCH
+//**********************************************************************************
+    gsap.set(".projetFromProjets", {y: 100});
+
+    ScrollTrigger.batch(".projetFromProjets", {
+        //interval: 0.1, // time window (in seconds) for batching to occur.
+        onEnter: batch => gsap.to(batch, {opacity: 1, y: 0, stagger: {each: 0.15, grid: [1, 3]}, overwrite: true}),
+        onLeave: batch => gsap.set(batch, {opacity: 0, y: -100, overwrite: true}),
+        onEnterBack: batch => gsap.to(batch, {opacity: 1, y: 0, stagger: 0.15, overwrite: true}),
+        onLeaveBack: batch => gsap.set(batch, {opacity: 0, y: 100, overwrite: true})
+    });
+    
+    ScrollTrigger.addEventListener("refreshInit", () => gsap.set(".image", {y: 0}));
     }
 });
   
